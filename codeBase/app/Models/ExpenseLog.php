67ccffb7982ea8dd3c3116deb25445da84036d2e
ayoubsalihi <ayoubsqlehi50@gmail.com>
2025-11-12 17:18:40 +0000

    model ExpenseLog completed

---
+++ codeBase/app/Models/ExpenseLog.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ExpenseLog extends Model
+{
+    /** @use HasFactory<\Database\Factories\ExpenseLogFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'expense_id',
+        'user_id',
+        'from_status',
+        'to_status',
+    ];
+
+    public function expense()
+    {
+        return $this->belongsTo(Expense::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
